1dbe642d9f625cfb717a5754f22985ba17b4c155
Prabhjot Singh <101247146+PrabhGhai@users.noreply.github.com>
2023-01-24 11:53:58 +0530

    first commit

--- frontend/src/components/Homeheader.js
+++ frontend/src/components/Homeheader.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Homeheader = () => {
+  const dt = new Date();
   return (
     <div>
       <div
@@ -10,7 +11,8 @@ const Homeheader = () => {
         <div className="container d-flex justify-content-center align-items-center flex-column ">
           <h1 className="text-white header">Let's Start Blogging</h1>
           <h5 className="text-white">
-            Write the blogs in simple way and share with the world in 2022.
+            Write the blogs in simple way and share with the world in
+            {dt.getFullYear()}.
           </h5>
         </div>
       </div>
