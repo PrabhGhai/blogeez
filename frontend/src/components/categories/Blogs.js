e27811d76a62a780373da6f2f0f083fb4f72d950
Prabhjot Singh <101247146+PrabhGhai@users.noreply.github.com>
2023-02-25 16:36:49 +0530

    thiteenth commit

--- frontend/src/components/categories/Blogs.js
+++ frontend/src/components/categories/Blogs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { CgProfile } from "react-icons/cg";
 import { Link } from "react-router-dom";
 const Blogs = () => {
   const [Blog, setBlog] = useState();
